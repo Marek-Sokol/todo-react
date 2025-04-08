954634125d0d91b134ccf7c0ed75bc32ec8a8360
Marek Sokol <mareksokol@Mareks-MacBook-Pro.local>
2025-04-08 21:30:45 +0200

    fix import in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
 export default {
   content: [
     './index.html',
@@ -20,8 +22,7 @@ export default {
       },
     },
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
     themes: false,
   },
